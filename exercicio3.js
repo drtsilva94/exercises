9ff2fe738cb7512a73c98791e637d58a6c6a315a
drtsilva94 <119511971+drtsilva94@users.noreply.github.com>
2023-08-08 19:10:24 -0300

    Add exercise 3

---
+++ exercicio3.js
@@ -0,0 +1,6 @@
+//Fazer uma função que recebe três strings e me retorne um array contendo todas elas
+
+function frutas(abacate,banana,uva){
+    var result = [abacate,banana,uva];
+    return result;
+}
