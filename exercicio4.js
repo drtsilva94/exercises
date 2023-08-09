4e7dece780d88e35e572189e37adb0bd3fb9f11f
drtsilva94 <119511971+drtsilva94@users.noreply.github.com>
2023-08-09 20:24:42 -0300

    Add exercise 4

---
+++ exercicio4.js
@@ -0,0 +1,6 @@
+//Faça uma função chamada somaNumeros que retorna o resultado da soma de dois números
+
+function somaNumeros(numero1,numero2){
+   var result = numero1 + numero2
+    return result
+}
